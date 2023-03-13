f7eadd00744763d021a98079efb76e453e45ea76
AnaisFarr <anais.farr@ens-rennes.fr>
2023-03-13 17:10:01 -0400

    Create new code save + update README

--- Code - examples/visualisation.py
+++ Code - examples/visualisation.py
@@ -1,4 +1,40 @@
 import bioviz
+import pickle
+import sys
+sys.path.append("/home/lim/Documents/Anais/bioviz")
+sys.path.append("/home/lim/Documents/Anais/bioptim")
 
-b = bioviz.Viz("/home/lim/Documents/Anais/Robust_standingBack/Model/Model2D_8Dof_2C_5M.bioMod", show_floor=True, show_meshes=True)
-b.exec()
+#name_file_pickle = "/home/lim/Anais/Results_opti/Data/Salto_6phases/Salto_6phases_V14"
+#name_file = "/home/lim/Documents/Anais/Robust_standingBack/Model/Model2D_8Dof_2C_5M.bioMod"
+# def get_pickle(file: str):
+#       with open(file, "rb") as file:
+#             while True:
+#                   try:
+#                         data_tmp = pickle.load(file)
+#                   except:
+#                         break
+#
+
+def get_created_data_from_pickle(file: str):
+      with open(file, "rb") as file:
+            while True:
+                  try:
+                        data_tmp = pickle.load(file)
+                  except:
+                        break
+
+      datas_q = data_tmp["states"][1]
+      #datas_qdot = data_tmp["states"]["qdot"]
+      #datas_tau = data_tmp["controls"]["tau"]
+      #data_status = data_tmp["status"]
+      #data_mus = data_tmp["controls"]["muscles"]
+      #data_time = data_tmp["real_time_to_optimize"]
+      #data_it = data_tmp["iterations"]
+      #data_cost = data_tmp["detailed_cost"]
+
+      return datas_q #datas_qdot, datas_tau, data_status, data_mus, data_it, data_time, data_cost
+
+q = get_created_data_from_pickle("/home/lim/Anais/Results_opti/Data/Salto_6phases/Salto_6phases_V15.pkl")
+#b = bioviz.Viz("/home/lim/Documents/Anais/Robust_standingBack/Model/Model2D_8Dof_2C_5M.bioMod", show_floor=True, show_meshes=False)
+#b.load_movement(q)
+#b.exec()
--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # Robust_standingBack
 This repo aims to generate robust techniques for a standing back somersault on ground.
 
+## Steps
+In first, I create codes to generate simple jump in two (`Salto_2phases.py`) and 
+three phases(`Salto_3phases.py`) in order to learn how to use bioptim.
+
+The second step was to add a backward somersault during the flight phase, when the model attain the maximal height of 
+his centre of mass (CoM). I begin by implement a backward somersault in a 3 phases movement (`Salto_3phases.py`) 
+and gradually I increase the number of phases until reach 6 phases(`Salto_6phases.py`).
+
+Add step actuator
+
+
 ## Model
+
+Create model 2D from 3D model with `GeneratePlanarModel.py`.
+
 
 The different model use are in the folder "Model". 
 The name of the model is defines like this: `"Model2D_8Dof_2C_3M"` according to the type of the model (i.e. 2D or 3D), the number of degree of freedom (i.e. Dof) 
 the number of contact (i.e. C), the number of markers (i.e. M)
+
+## How to save data
+
+To save data, we use `Save.py`. 
+
+
+##
 
