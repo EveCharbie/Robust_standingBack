1055e7c93686c360ac661d91ee3dc7cbecfbd393
Anais <anais.farr@ens-rennes.fr>
2024-07-20 06:55:24 -0400

    Salto_close_loop_landing_5phases_V39: INFAISABLE !
    Modification model + Contraint pied première phase

--- holonomic_research/Salto_5phases_CL_with_pelvis_landing.py
+++ holonomic_research/Salto_5phases_CL_with_pelvis_landing.py
@@ -241,7 +241,7 @@ def custom_phase_transition_post(
 
 # --- Parameters --- #
 movement = "Salto_close_loop_landing"
-version = 38
+version = 39
 nb_phase = 5
 name_folder_model = "/home/mickaelbegon/Documents/Anais/Robust_standingBack/Model"
 
@@ -341,15 +341,15 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
     holonomic_constraints = HolonomicConstraintsList()
 
     # Phase 0 (Propulsion):
-    #constraints.add(
-    #    ConstraintFcn.TRACK_MARKERS,
-    #    marker_index="Foot_Toe",
-    #    axes=Axis.Z,
-    #    max_bound=0,
-    #    min_bound=0,
-    #    node=Node.START,
-    #    phase=0,
-    #)
+    constraints.add(
+        ConstraintFcn.TRACK_MARKERS,
+        marker_index="Foot_Toe_marker",
+        axes=Axis.Z,
+        max_bound=0,
+        min_bound=0,
+        node=Node.START,
+        phase=0,
+    )
 
     #constraints.add(
     #    CoM_over_toes,
