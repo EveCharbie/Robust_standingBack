9868d11a3e4d3ad8c80810c3e5b495012054f790
Anais <anais.farr@ens-rennes.fr>
2024-07-22 11:23:41 -0400

    Salto_close_loop_landing_5phases_V42: INFAISABLE !
    Ajout contrainte pied Y + remove contraints Z pied

--- holonomic_research/Salto_5phases_CL_with_pelvis_landing.py
+++ holonomic_research/Salto_5phases_CL_with_pelvis_landing.py
@@ -241,7 +241,7 @@ def custom_phase_transition_post(
 
 # --- Parameters --- #
 movement = "Salto_close_loop_landing"
-version = 41
+version = 42
 nb_phase = 5
 name_folder_model = "/home/mickaelbegon/Documents/Anais/Robust_standingBack/Model"
 
@@ -341,16 +341,16 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
     holonomic_constraints = HolonomicConstraintsList()
 
     # Phase 0 (Propulsion):
-    constraints.add(
-        ConstraintFcn.TRACK_MARKERS,
-        marker_index="Foot_Toe_marker",
-        axes=Axis.Z,
+    #constraints.add(
+    #    ConstraintFcn.TRACK_MARKERS,
+    #    marker_index="Foot_Toe_marker",
+    #    axes=Axis.Z,
         #max_bound=0,
         #min_bound=0,
-        target=0,
-        node=Node.START,
-        phase=0,
-    )
+    #    target=0,
+    #    node=Node.START,
+    #    phase=0,
+    #)
 
     constraints.add(
         CoM_over_toes,
@@ -435,15 +435,15 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
     #    phase=4,
     #)
 
-    #constraints.add(
-    #    ConstraintFcn.TRACK_MARKERS,
-    #    marker_index="Foot_Toe_marker",
-    #    axes=Axis.Y,
-    #    max_bound=0.1,
-    #    min_bound=-0.1,
-    #    node=Node.END,
-    #    phase=4,
-    #)
+    constraints.add(
+        ConstraintFcn.TRACK_MARKERS,
+        marker_index="Foot_Toe_marker",
+        axes=Axis.Y,
+        max_bound=0.1,
+        min_bound=-0.1,
+        node=Node.END,
+        phase=4,
+    )
 
     constraints.add(
         CoM_over_toes,
