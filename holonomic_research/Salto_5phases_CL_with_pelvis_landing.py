a2973eceb0ca9b7f6985c7445bec96a3c1412934
Anais <anais.farr@ens-rennes.fr>
2024-08-13 08:44:44 -0400

    Salto_close_loop_landing_5phases_V69: RESTORATION FAILED !
    Position de départ +0.3

--- holonomic_research/Salto_5phases_CL_with_pelvis_landing.py
+++ holonomic_research/Salto_5phases_CL_with_pelvis_landing.py
@@ -326,7 +326,7 @@ def custom_contraint_lambdas_cisaillement_2(
 
 # --- Parameters --- #
 movement = "Salto_close_loop_landing"
-version = 68
+version = 69
 nb_phase = 5
 name_folder_model = "/home/mickaelbegon/Documents/Anais/Robust_standingBack/Model"
 
@@ -339,6 +339,7 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
                  BiorbdModel(biorbd_model_path[3]),
                  BiorbdModel(biorbd_model_path[4]),
                  )
+
 
     tau_min_total = [0, 0, 0, -325.531, -138, -981.1876, -735.3286, -343.9806]
     tau_max_total = [0, 0, 0, 325.531, 138, 981.1876, 735.3286, 343.9806]
@@ -569,8 +570,9 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
     x_bounds = BoundsList()
     x_bounds.add("q", bounds=bio_model[1].bounds_from_ranges("q"), phase=0)
     x_bounds.add("qdot", bounds=bio_model[1].bounds_from_ranges("qdot"), phase=0)
-    x_bounds[0]["q"].min[:, 0] = np.array(pose_propulsion_start) - 0.5
-    x_bounds[0]["q"].max[:, 0] = np.array(pose_propulsion_start) + 0.5
+    x_bounds[0]["q"].min[:, 0] = np.array(pose_propulsion_start) - 0.3
+    x_bounds[0]["q"].max[:, 0] = np.array(pose_propulsion_start) + 0.3
+    x_bounds[0]["q"].max[2, 0] = 0.5
     x_bounds[0]["qdot"][:, 0] = [0] * n_qdot
     x_bounds[0]["q"].min[2, 1:] = -np.pi
     x_bounds[0]["q"].max[2, 1:] = np.pi
