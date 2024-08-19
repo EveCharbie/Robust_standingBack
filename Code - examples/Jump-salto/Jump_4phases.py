8c46e176370d57ec16abc523c77203e306a951cb
Anais <anais.farr@ens-rennes.fr>
2024-08-19 13:08:54 -0400

    Jump_4phases_V9: CONVERGED !
    Contraint ALL SHOOTING première phase + pas de borne pied première node première phase

--- Code - examples/Jump-salto/Jump_4phases.py
+++ Code - examples/Jump-salto/Jump_4phases.py
@@ -153,7 +153,7 @@ def CoM_over_toes(controller: PenaltyController) -> cas.MX:
 
 # --- Parameters --- #
 movement = "Jump"
-version = 8
+version = 9
 nb_phase = 4
 name_folder_model = "/home/mickaelbegon/Documents/Anais/Robust_standingBack/Model"
 
@@ -322,7 +322,7 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
 
     constraints.add(
         ConstraintFcn.NON_SLIPPING,
-        node=Node.ALL_SHOOTING,
+        node=Node.START,
         normal_component_idx=1,
         tangential_component_idx=0,
         static_friction_coefficient=0.5,
@@ -349,8 +349,8 @@ def prepare_ocp(biorbd_model_path, phase_time, n_shooting, min_bound, max_bound)
     # Phase 0: Propulsion
     x_bounds.add("q", bounds=bio_model[0].bounds_from_ranges("q"), phase=0)
     x_bounds.add("qdot", bounds=bio_model[0].bounds_from_ranges("qdot"), phase=0)
-    x_bounds[0]["q"].min[:, 0] = np.array(pose_propulsion_start) - 0.3
-    x_bounds[0]["q"].max[:, 0] = np.array(pose_propulsion_start) + 0.3
+    x_bounds[0]["q"].min[2:7, 0] = np.array(pose_propulsion_start[2:7]) - 0.2
+    x_bounds[0]["q"].max[2:7, 0] = np.array(pose_propulsion_start[2:7]) + 0.2
     #x_bounds[0]["q"].max[2, 0] = 0.5
     #x_bounds[0]["q"].max[5, 0] = 2
     #x_bounds[0]["q"].min[6, 0] = -2
